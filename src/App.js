e8399ce5edbbb23899c06c82ad4da525008c5771
gamemore <maison.chen@benq.com>
2017-08-21 07:30:54 +0000

    add time display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+    function tick() {
+        const element = (
+            <div>
+                <h1> hello, world!</h1>
+                <h2> It is {new Date().toString()}.</h2>
+            </div>
+        );
+
+        ReactDOM.render(
+            element,
+            document.getElementById('root')
+        );
+    };
+setInterval(tick, 1000);
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -13,9 +30,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> hello world</p>
       </div>
     );
   }
 }
 
 export default App;
+
