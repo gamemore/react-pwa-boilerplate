35c286e5c9c5b4ff1b5313c72f9fc67bc9c78ee6
gamemore <maison.chen@benq.com>
2017-08-22 08:18:34 +0000

    set state

--- src/App.js
+++ src/App.js
@@ -3,81 +3,55 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function show(){
-    function formatDate(date) {
-        return date.toLocaleDateString();
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
     }
 
-    function Avatar(props) {
-        return (
-            <img className="Avatar"
-                src={props.user.avatarUrl}
-                alt={props.user.name} />
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),// means function () { this.tick(); }
+            1000
         );
     }
 
-    function UserInfo(props) {
-        return (
-            <div className="UserInfo">
-                <Avatar user={props.user} />
-                <div className="UserInfo-name">
-                    {props.user.name}
-                </div>
-            </div>
-        );
+    componentWillUnmount() {
+        clearInterval(this.timerID);
     }
 
-    function Comment(props) {
-        return (
-            <div className="Comment">
-                <UserInfo user={props.author} />
-                <div className="Comment-text">
-                    {props.text}
-                </div>
-                <div className="Comment-date">
-                    {formatDate(props.date)}
-
-                </div>
-            </div>
-        );
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
-
-    const comment = {
-        date: new Date(),
-        text: 'I hope you enjoy learning React!',
-        author: {
-            name: 'Maison Chen',
-            avatarUrl: 'http://placekitten.com/g/64/64'
-        }
-    };
-
-    ReactDOM.render(
-        <Comment 
-            date={comment.date}
-            text={comment.text}
-            author={comment.author} />,
-        document.getElementById('root')
-    );
-}
-setInterval(show, 1000);
-
-class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> hello world</p>
+      <div>
+        <h1> Hello, World! </h1>
+        <h2> It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
-export default App;
+function show() {
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
+}
 
+setTimeout(show, 1000);
+
+class App extends Component {
+    render () {
+        return (
+            <div> original screen </div>
+        );
+    }
+
+}
+export default App;
