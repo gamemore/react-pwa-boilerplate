c6a9d35b4f50bc03daa00accbf43174427f83d9e
gamemore <maison.chen@benq.com>
2017-08-22 07:11:15 +0000

    display a comment with user info

--- src/App.js
+++ src/App.js
@@ -4,22 +4,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function show(){
-    function Welcome(props) {
-        return <h1>Hello, {props.name}</h1>;
+    function formatDate(date) {
+        return date.toLocaleDateString();
     }
 
-    function App1() {
+    function Avatar(props) {
         return (
-            <div>
-                <Welcome name="Maison" />
-                <Welcome name="CW" />
-                <Welcome name="Chen" />
-            </div>
-        )
+            <img className="Avatar"
+                src={props.user.avatarUrl}
+                alt={props.user.name} />
+        );
     }
-    const element = <Welcome name="Maison" />;
+
+    function UserInfo(props) {
+        return (
+            <div className="UserInfo">
+                <Avatar user={props.user} />
+                <div className="UserInfo-name">
+                    {props.user.name}
+                </div>
+            </div>
+        );
+    }
+
+    function Comment(props) {
+        return (
+            <div className="Comment">
+                <UserInfo user={props.author} />
+                <div className="Comment-text">
+                    {props.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(props.date)}
+
+                </div>
+            </div>
+        );
+    }
+
+    const comment = {
+        date: new Date(),
+        text: 'I hope you enjoy learning React!',
+        author: {
+            name: 'Maison Chen',
+            avatarUrl: 'http://placekitten.com/g/64/64'
+        }
+    };
+
     ReactDOM.render(
-        <App1 />,
+        <Comment 
+            date={comment.date}
+            text={comment.text}
+            author={comment.author} />,
         document.getElementById('root')
     );
 }
